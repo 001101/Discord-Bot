9317d97d3efee3f94d10cc2e6b5251918b732845
Lala Sabathil <pkmoras.facebook@web.de>
2018-10-13 12:46:06 +0200

    tester missed

---
+++ tester.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo -e "WARNING!!\nYou need to pass the WEBHOOK_URL environment variable as the second argument to this script.\n" && exit
+fi
+
+echo -e "[Webhook]: Sending webhook to Discord...\\n";
+
+echo -e "[Webhook]: Informing Testers...\\n";
+
+WEBHOOK_DATA='{
+  "username": "",
+  "content": "!notify tester",
+  "avatar_url": "https://cdn.discordapp.com/attachments/497572862467768320/500299505875288064/TECH_NEWS_ICON_1080x1080_FINAL.jpg"
+}'
+
+(curl --fail --progress-bar -A "HC-Tester" -H Content-Type:application/json -H X-Author:"Lala Sabathil#7777" -d "$WEBHOOK_DATA" "$2" \
+  && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook."
