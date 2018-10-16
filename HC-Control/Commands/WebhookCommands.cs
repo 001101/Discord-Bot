14fbd0d478d49bceb7265127399796707a41b3d9
Lala Sabathil <pkmoras.facebook@web.de>
2018-10-16 18:33:33 +0200

    Notify Command

---
+++ HC-Control/Commands/WebhookCommands.cs
@@ -0,0 +1,21 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System.Threading.Tasks;
+
+namespace HC_Control.Commands
+{
+    class WebhookCommands : BaseCommandModule
+    {
+
+        [Command("notifyt"), RequirePrefixes("!"), RequireGuild()]
+        public async Task Translate(CommandContext ctx, [RemainingText] string info = null)
+        {
+            DiscordGuild guild = ctx.Guild;
+            DiscordRole role = guild.GetRole(499915116826066944);
+            DiscordChannel chan = guild.GetChannel(499915455318982657);
+            await chan.SendMessageAsync($"Hello {role.Mention}. A new build seems to exsist. Please test!", false, null);
+            await ctx.Message.DeleteAsync();
+        }
+    }
+}
